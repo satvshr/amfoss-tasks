31b5110828de38980e59f7cb2e533e0ff18934df
satvshr <112589278+satvshr@users.noreply.github.com>
2022-12-14 03:08:23 +0530

    solved Deadshot

--- Task-03/Deadshot.py
+++ Task-03/Deadshot.py
@@ -1,7 +1,10 @@
 n = int(input())
 c = []
 ans = 0
-
+p = 1
+q = 2
+r = 3
+s = 4
 for i in range(n):
     a = list(map(int,input().split()))
     c.append(a)
@@ -10,26 +13,25 @@ for i in range(n):
 
 
 for z in c:
+    d = []
     counter = 0
     for i in c:
       #  print([z[0],i[0]],[z[1],i[1]])
     
         if (z[0] == i[0]) and (z[1] > i[1]):
-            counter += 1
-            continue
+            d.append(p)
         elif (z[0] == i[0]) and (z[1] < i[1]):
-            counter += 1
-            continue
+            d.append(q)
         elif (z[0] < i[0]) and (z[1] == i[1]):
-            counter += 1
-            continue
+            d.append(r)
         elif (z[0] > i[0]) and (z[1] == i[1]):
-            counter += 1
-            continue
-    #print(counter)
-    if counter == 5:
-            ans += 1
-            continue
+            d.append(s)
+        if p in d:
+            if q in d:
+                if r in d:
+                    if s in d:
+                        ans+= 1
+                        break
 print(ans)
 
 # for x in c:
